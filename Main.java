2b2b06a187de1201458d0a55475f88757b11ec37
SuhaniSP <suhani.s.patrimath@gmail.com>
2025-04-25 08:52:32 +0530

    Implemented add-student method

--- Main.java
+++ Main.java
@@ -9,3 +9,15 @@ public static void addStudent() {
     students.add(new Student(id, name, grade));
     System.out.println("Student added successfully.");
 }
+
+static void addStudent() {
+    System.out.print("Enter Student ID: ");
+    int id = sc.nextInt();
+    sc.nextLine(); // Clear buffer
+    System.out.print("Enter Student Name: ");
+    String name = sc.nextLine();
+    System.out.print("Enter Student Grade: ");
+    String grade = sc.nextLine();
+    students.add(new Student(id, name, grade));
+    System.out.println("Student added successfully.");
+    }
