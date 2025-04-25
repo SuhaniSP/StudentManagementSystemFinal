6ef764a6a4b684374438b6e39403b9baf868d2fa
SuhaniSP <suhani.s.patrimath@gmail.com>
2025-04-25 12:14:50 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,4 @@
-// Inside Main class
-public static void addStudent() {
-    System.out.print("Enter ID: ");
-    int id = sc.nextInt();
-    System.out.print("Enter Name: ");
-    String name = sc.next();
-    System.out.print("Enter Grade: ");
-    String grade = sc.next();
-    students.add(new Student(id, name, grade));
-    System.out.println("Student added successfully.");
-}
+
 
 static void addStudent() {
     System.out.print("Enter Student ID: ");
